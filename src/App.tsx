6ddc5b9453cecae14f6667b52ef15ed40ecf7fdf
williamjpriest <liuliu6wp@gmail.com>
2023-10-07 04:23:53 -0400

    first socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import {useState} from 'react'
+import { io } from 'socket.io-client';
 
 function App() {
   const [message,setMessage] = useState();
+  const URL = 'http://localhost:3000';
+  const socket = io(URL);
+  console.log(socket)
 
   const changeMessage = (e:any) =>{
     setMessage(e.target.value)
@@ -9,7 +13,9 @@ function App() {
   }
   const sendBTN = async() => {
     console.log(message)
+    console.log(socket)
   }
+
 
   return (
     <>
