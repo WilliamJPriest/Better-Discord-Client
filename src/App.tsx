40c30bd66f61717cd55caebd8c0daf53d4d62852
williamjpriest <liuliu6wp@gmail.com>
2023-10-08 03:29:44 -0400

    two way sending

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import { io } from 'socket.io-client';
 
 function App() {
@@ -6,23 +6,29 @@ function App() {
   const URL = 'http://localhost:4000';
   const socket = io(URL);
 
+    useEffect(() =>{
+      socket.on('recmessage', (arg:any)=>{
+        console.log(arg)})   
+        return () => {
+          socket.close();
+        }
+      }, [socket]);
+
   const changeMessage = (e:any) =>{
     setMessage(e.target.value)
 
   }
-  const sendBTN = (e: any) => {
-    // e.preventDefault();    
-    socket.on('hello', (arg)=>{
-      console.log(arg,`
+  const sendBTN =  (e:any) => {
+    e.preventDefault()
 
-      ___  _____  ___  _  _  ____  ____    ____  ___  
-     / __)(  _  )/ __)( )/ )( ___)(_  _)  (_  _)/ _ \ 
-     \__ \ )(_)(( (__  )  (  )__)   )(     _)(_( (_) )
-     (___/(_____)\___)(_)\_)(____) (__)   (____)\___/ 
-     
-     
-            `)
-    });
+
+   try{
+      socket.emit("message", message)
+    }
+    catch(err){
+      console.log(err)
+    }
+
   }
 
 
@@ -30,8 +36,14 @@ function App() {
     <>
     <section>
       <article>
-        <input value={message} onChange={ e => changeMessage(e)} placeholder="send a message"></input>
-        <button onClick={(sendBTN)}></button>
+        
+
+      </article>
+      <article>
+        <form >
+          <input value={message} onChange={ e => changeMessage(e)} placeholder="send a message"></input>
+          <button onClick={(sendBTN)}>send</button>
+        </form>
       </article>
     </section>
     </>
