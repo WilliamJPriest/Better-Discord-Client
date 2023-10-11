6988211066d40a1e289280072bf9fede29b0812a
williamjpriest <liuliu6wp@gmail.com>
2023-10-11 10:40:49 -0400

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import {useEffect, useState} from 'react'
-import { io } from 'socket.io-client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css"
+import Messenger from "./Pages/Messenger";
 
 //todos
 // dynamic id sending instead of general sending
@@ -10,103 +10,18 @@ import "./App.css"
 // MultiPage?? can try Context API, possibly.
 
 function App() {
-  const [nameSet,setNameSet]= useState(false)
-  const [currentUser, setCurrentUser]= useState({
-      name: "",
-      message:"",
-      socket_id:"",
-      room:"",
-  })
-  
-  const [otherUser, setOtherUser]= useState({
-    name: "",
-    message:"",
-    socket_id:"",
-    room:"",
-})
 
-const [online,setOnline]= useState([])
-
-  const URL = 'http://localhost:4000';
-  const socket = io(URL);
-
-    useEffect(() =>{
-
-      socket.on('recmessage', (arg:any)=>{
-        setOtherUser(arg)})
-        
-      socket.on('onlineU', (duh:any)=>{
-        setOnline(duh)
-        console.log(duh)
-      })   
-
-        return () => {
-          socket.close();
-        }
-      }, [socket]);
-
-  const changeHandler = async(e:any) =>{
-    setCurrentUser({
-      ...currentUser,
-      [e.target.name]: e.target.value,
-    
-  })  
-}
-
-  const sendBTN =  (e:any) => {
-    e.preventDefault()
-
-
-   try{
-      socket.emit("message", currentUser)
-    }
-    catch(err){
-      console.log(err)
-    }
-
-  }
-
-  const setNameBTN =  (e:any) => {
-    e.preventDefault();
-    setNameSet(true)
-    socket.emit("online", currentUser.name)
-
-  }
-
-return (
-  <>
-
-  {nameSet ?   <section className='flex justify-center text-center mx-auto'>
-    <article> 
-      <p className='color-red-800'>{otherUser.name} {otherUser.message}</p>
-    </article>
-    <article>
-      <form >
-        <input value={currentUser.message} onChange={changeHandler} name="message" placeholder="send a message"></input>
-        <button onClick={(sendBTN)}>send</button>
-        
-      </form>
-      <p>{currentUser.name}{online}</p>
-    </article>
-    
-  </section> 
-  : 
-  <section className='flex justify-center text-center mx-auto'>
-    {/* <article> 
-      <p className='color-red-800'>{recMessage}</p>
-    </article> */}
-    <article>
-      <form >
-        <input value={currentUser.name} onChange={changeHandler} name="name" placeholder="set name"></input>
-        <button onClick={(setNameBTN)}>set name</button>
-      </form>
-    </article>
-  </section>
-}
-
-      
-  </>
-)
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Messenger/>}/>
+        {/* <Route path='*' element={< />}/>
+        <Route path='/unsubscriber' element={<Unsubscriber />}/> */}
+      </Routes>    
+    </BrowserRouter>
+    </>
+  )
 }
 
 export default App
